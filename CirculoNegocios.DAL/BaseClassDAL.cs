75cb1266238fa6a275f8d64207f355a6c8744350
Marcio <macousa@gmail.com>
2014-05-28 23:44:50 -0300

    Create
    Criando a solução compartilhada

---
+++ CirculoNegocios.DAL/BaseClassDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using System.Configuration;
+using System.Data;
+
+namespace CirculoNegocios.DAL
+{
+    public class BaseClassDAL
+    {
+        public IDBManager dbManager = new DBManager(DataProvider.SqlServer) { ConnectionString = ConfigurationManager.ConnectionStrings["CircNegocioCon"].ToString() };
+    }
+}
---
+++ CirculoNegocios.DAL/LogDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CirculoNegocios.DAL
+{
+    public class LogDAL : BaseClassDAL
+    {
+        public void InsereLog(string tabela, string acao, string valorAntes, string valorDepois)
+        { 
+            
+        }
+    }
+}
